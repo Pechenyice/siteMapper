fa8396a7458b9e2eb30651e1a64a6648b9ce5580
pechenyice <german.iskhakov@mail.ru>
2021-10-21 20:23:04 +0300

    added layout styling

--- src/noRedux/elements/components/FolderElem/FolderElem.tsx
+++ src/noRedux/elements/components/FolderElem/FolderElem.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {IState as PProps} from './../../TSRootApp';
 
 interface IProps {
-    data: PProps["folder"]
+    data: PProps["folder"],
+    index: number
 }
 
-const folderElem: React.FC<IProps>  = ({data}) => {
+const folderElem: React.FC<IProps>  = ({data, index}) => {
     return (
             <div>
+                folder {index}
                 <p>name: {data.name}</p>
             </div>
     );
