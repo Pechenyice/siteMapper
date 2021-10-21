a4ac4321cd3333a729ffcfde33020d537ee8b902
pechenyice <german.iskhakov@mail.ru>
2021-10-21 14:16:07 +0300

    updated root element

--- src/noRedux/elements/TSRootApp.tsx
+++ src/noRedux/elements/TSRootApp.tsx
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
+import ContentDisplay from "./components/ContentDisplay/ContentDisplay";
 import ModalManager from "./components/ModalManager/ModalManager";
 
 export interface IState {
-    link: {
-        name: string,
-        url: string,
-        folder: string
-    },
-    folder: {
-        name: string
-    }
+	link: {
+		name: string;
+		url: string;
+		folder: string;
+	};
+	folder: {
+		name: string;
+	};
 }
 
 function TSRootApp() {
+	let [links, setLinks] = useState<IState["link"][]>([]);
 
-    let [links, setLinks] = useState<IState["link"][]>([]);
+	let [folders, setFolders] = useState<IState["folder"][]>([
+		{ name: "default" },
+	]);
 
-    let [folders, setFolders] = useState<IState["folder"][]>([{name: 'default'}]);
-
-    return (
-        <>
-            <ModalManager 
-                setFolders={setFolders} 
-                folders={folders}
-                setLinks={setLinks} 
-                links={links}
+	return (
+		<>
+			<ContentDisplay 
+                folders={folders} 
+                links={links} 
             />
-        </>
-    );
+			<ModalManager
+				setFolders={setFolders}
+				folders={folders}
+				setLinks={setLinks}
+				links={links}
+			/>
+		</>
+	);
 }
 
 export default TSRootApp;
