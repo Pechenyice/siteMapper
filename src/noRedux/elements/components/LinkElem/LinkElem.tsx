3426f694cfa10541e55a7450d93186fad04a4c39
pechenyice <german.iskhakov@mail.ru>
2021-10-21 20:23:32 +0300

    added layout styling

--- src/noRedux/elements/components/LinkElem/LinkElem.tsx
+++ src/noRedux/elements/components/LinkElem/LinkElem.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import {IState as PProps} from './../../TSRootApp';
 
 interface IProps {
-    data: PProps["link"]
+    data: PProps["link"],
+    index: number
 }
 
-const LinkElem: React.FC<IProps>  = ({data}) => {
+const LinkElem: React.FC<IProps>  = ({data, index}) => {
     return (
         <a href={data.url}>
             <div>
+                Favorite {index}
                 <p>name: {data.name}</p>
                 <p>folder: {data.folder}</p>
             </div>
