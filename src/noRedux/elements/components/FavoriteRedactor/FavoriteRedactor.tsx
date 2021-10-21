770c51031030688ca1c8c995383f153120fcfb56
pechenyice <german.iskhakov@mail.ru>
2021-10-21 20:22:58 +0300

    added layout styling

--- src/noRedux/elements/components/FavoriteRedactor/FavoriteRedactor.tsx
+++ src/noRedux/elements/components/FavoriteRedactor/FavoriteRedactor.tsx
@@ -3,9 +3,11 @@ import {IState as PProps} from './../../TSRootApp';
 
 interface IProps {
     handleAddLink: (link: PProps["link"]) => void
+    handleAddFolder: (link: PProps["folder"]) => void
+    folders: PProps["folder"][]
 }
 
-const FavoriteRedactor: React.FC<IProps> = ({handleAddLink}) => {
+const FavoriteRedactor: React.FC<IProps> = ({handleAddLink, handleAddFolder, folders}) => {
 
     let [favorite, setFavorite] = useState<PProps["link"]>({
         name: '',
@@ -26,31 +28,35 @@ const FavoriteRedactor: React.FC<IProps> = ({handleAddLink}) => {
     }
 
     function handleFavoriteCreate() {
+        if (!folders.filter(e => e.name === favorite.folder).length) handleAddFolder({name: favorite.folder});
         handleAddLink(favorite);
     }
 
     return (
         <section className="main__favoriteRedactor main__redactor">
             <div className="redactor__wrapper">
+            <label htmlFor="name">Favorite name: </label>
 				<input
 					type="text"
 					value={favorite.name}
                     name={'name'}
 					onChange={handleInputChange}
 				></input>
+                <label htmlFor="url">Favorite url: </label>
                 <input
 					type="text"
 					value={favorite.url}
                     name={'url'}
 					onChange={handleInputChange}
 				></input>
+                <label htmlFor="folder">Favorite folder: </label>
                 <input
 					type="text"
 					value={favorite.folder}
                     name={'folder'}
 					onChange={handleInputChange}
 				></input>
-				<button onClick={handleFavoriteCreate}>create folder</button>
+				<button onClick={handleFavoriteCreate}>create favorite</button>
 			</div>
         </section>
     );
