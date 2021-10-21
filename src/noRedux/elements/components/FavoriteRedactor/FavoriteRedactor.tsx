34925cc986f752382f17b71d300820a2a4dde02d
pechenyice <german.iskhakov@mail.ru>
2021-10-21 14:17:15 +0300

    updated redactor of favorites component

--- src/noRedux/elements/components/FavoriteRedactor/FavoriteRedactor.tsx
+++ src/noRedux/elements/components/FavoriteRedactor/FavoriteRedactor.tsx
@@ -12,10 +12,46 @@ const FavoriteRedactor: React.FC<IProps> = ({handleAddLink}) => {
         url: '',
         folder: 'default'
     });
+    
+    function handleInputChange(e : React.ChangeEvent<HTMLInputElement>) {
+        setFavorite(
+            Object.assign(
+                {},
+                favorite,
+                {
+                    [e.target.name]: e.target.value
+                }
+            )
+        );
+    }
+
+    function handleFavoriteCreate() {
+        handleAddLink(favorite);
+    }
 
     return (
         <section className="main__favoriteRedactor main__redactor">
-            favorite redactor
+            <div className="redactor__wrapper">
+				<input
+					type="text"
+					value={favorite.name}
+                    name={'name'}
+					onChange={handleInputChange}
+				></input>
+                <input
+					type="text"
+					value={favorite.url}
+                    name={'url'}
+					onChange={handleInputChange}
+				></input>
+                <input
+					type="text"
+					value={favorite.folder}
+                    name={'folder'}
+					onChange={handleInputChange}
+				></input>
+				<button onClick={handleFavoriteCreate}>create folder</button>
+			</div>
         </section>
     );
 }
