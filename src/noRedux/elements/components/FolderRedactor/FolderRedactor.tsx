2117c83db23610e2357f0494a130f4ff9377ca1b
pechenyice <german.iskhakov@mail.ru>
2021-10-21 14:17:49 +0300

    updated redactor of folders component

--- src/noRedux/elements/components/FolderRedactor/FolderRedactor.tsx
+++ src/noRedux/elements/components/FolderRedactor/FolderRedactor.tsx
@@ -1,18 +1,37 @@
-import React from "react";
-import {IState as PProps} from './../../TSRootApp';
+import React, { useState } from "react";
+import { IState as PProps } from "./../../TSRootApp";
 
 interface IProps {
-    handleAddFolder: (link: PProps["folder"]) => void
+	handleAddFolder: (link: PProps["folder"]) => void;
 }
 
-// another case 
+// another case
 
-function FolderRedactor({handleAddFolder} : IProps) {
-    return (
-        <section className="main__folderRedactor main__redactor">
-            folder redactor
-        </section>
-    );
+function FolderRedactor({ handleAddFolder }: IProps) {
+	let [folder, setFolder] = useState<string>("");
+
+	function handleFolderChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setFolder(e.target.value);
+	}
+
+	function handleFolderCreate() {
+		handleAddFolder({
+			name: folder,
+		});
+	}
+
+	return (
+		<section className="main__folderRedactor main__redactor">
+			<div className="redactor__wrapper">
+				<input
+					type="text"
+					value={folder}
+					onChange={handleFolderChange}
+				></input>
+				<button onClick={handleFolderCreate}>create folder</button>
+			</div>
+		</section>
+	);
 }
 
 export default FolderRedactor;
