0ce293d5434bfea839b8994faecb89b678af3113
pechenyice <german.iskhakov@mail.ru>
2021-10-21 20:22:47 +0300

    added layout styling

--- src/noRedux/elements/components/ContentDisplay/ContentDisplay.tsx
+++ src/noRedux/elements/components/ContentDisplay/ContentDisplay.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import FoldersDisplay from "../FoldersDisplay/FoldersDisplay";
 import LinksDisplay from "../LinksDisplay/LinksDisplay";
-import {IState as PProps} from './../../TSRootApp';
+import { IState as PProps } from "./../../TSRootApp";
 
 interface IProps {
-    links: PProps["link"][],
-    folders: PProps["folder"][],
+	links: PProps["link"][];
+	folders: PProps["folder"][];
 }
 
-const ContentDisplay: React.FC<IProps>  = ({links, folders}) => {
-    return (
-        <>
-            <LinksDisplay links={links}/>
-            <FoldersDisplay folders={folders}/>
-        </>
-    );
-}
+const ContentDisplay: React.FC<IProps> = ({ links, folders }) => {
+	return (
+		<section className="main__displayWrapper">
+			<section className="main__display">
+				<LinksDisplay links={links} />
+				<FoldersDisplay folders={folders} />
+			</section>
+		</section>
+	);
+};
 
 export default ContentDisplay;
